007961b0ceab5f5e5fb2195ebb9c0d8e1a8920ca
Maarten Timmerman <maarten.timmerman@city-live.be>
2011-04-20 13:31:40 +0200

    changed fields

--- src/django_votes/models.py
+++ src/django_votes/models.py
@@ -1,25 +1,94 @@
 from django.db import models
 from django.db.models.base import ModelBase
 from django.utils.translation import (ugettext_lazy as _, ugettext)
+from django.contrib.auth.models import User
 
-# Global list of all state models (mapping from name -> model class }
+class VotesField(object):
+    """
+    Usage:
+
+    class MyModel(models.Model):
+        ...
+        Votes = VotesField()
+    """
+    def __init__(self):
+        pass
+
+    def contribute_to_class(self, cls, name):
+        self._name = name
+        models.signals.class_prepared.connect(self.finalize, sender=cls)
+
+    def finalize(self, sender, **kwargs):
+        descriptor = self._create_Vote_model(sender)
+        setattr(sender, self._name, descriptor)
+
+    def _create_Vote_model(self, model):
+        class VoteMeta(ModelBase):
+            """
+            Make every Vote model have their own name/table.
+            """
+            def __new__(c, name, bases, attrs):
+                # Rename class
+                name = '%sVote' % model._meta.object_name
+
+                # This attribute is required for a model to function properly in Django.
+                attrs['__module__'] = model.__module__
+
+                return ModelBase.__new__(c, name, bases, attrs)
+
+        rel_nm_user = '%s_votes' % model._meta.object_name.lower()
+
+        class Vote(models.Model):
+            """
+            Vote model
+            """
+            __metaclass__ = VoteMeta
+
+            voter = models.ForeignKey(User, verbose_name=_('voter'))
+            value = models.IntegerField(default=1, verbose_name=_('value'))            
+            date = models.DateField(db_index=True, verbose_name=_('voted on'))
+            object = models.ForeignKey(model, verbose_name=_('object'))
+            
+            class Meta:
+                ordering = ('date',)
+                verbose_name = _('Vote')
+                verbose_name_plural = _('Votes')
+
+            def __unicode__(self):
+                values = {'voter': self.voter.username,
+                          'like': _('likes') if self.value > 0 else _('hates'),
+                          'object': self.object}
+                
+                return "%(voter)s %(like)s %(object)s" % values
+
+
+        class VoteFieldDescriptor(object):
+            def __init__(self):
+                pass
+
+            def __get__(self, obj, objtype):
+                """
+                Return the related manager for the Votes.
+                """
+                if obj:
+                    return getattr(obj, ('%svote_set' % model._meta.object_name).lower())
+                else:
+                    return Vote.objects
+
+        return VoteFieldDescriptor()
         
 class VoteModel(models.Model):
     """
     Every model which needs votes should inherit this abstract model.
     """
     
-    up_votes = models.IntegerField(default=0, verbose_name=_('up votes'))
-    down_votes = models.IntegerField(default=0, verbose_name=_('down votes'))
+    votes = VotesField(verbose_name=_('votes'))
         
     class Meta:
         verbose_name = _('vote')
         verbose_name_plural = _('votes')
         abstract = True
-        
-    def __unicode__(self):
-        return 'Up votes: %s | Down votes: %s' % (self.up_votes, self.down_votes,)
-    
+            
     @classmethod
     def get_state_model_name(self):
         return '%s.%s' % (self._meta.app_label, self._meta.object_name)
