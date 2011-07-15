39e0738a5246ba9eeb4a9cd1f6e3a81de2d03c3a
Maarten Timmerman <maarten.timmerman@city-live.be>
2011-07-15 11:11:28 +0200

    styling

--- src/django_votes/templatetags/votes.py
+++ src/django_votes/templatetags/votes.py
@@ -5,6 +5,7 @@ from django.template.loader import render_to_string
 register = template.Library()
 
 from django_votes.models import VotesField
+from django_votes.utils import get_vote_model
 
 class UpDownVoteNode(template.Node):
     def __init__(self, object):
@@ -17,8 +18,22 @@ class UpDownVoteNode(template.Node):
         model_name = '%s.%sVote' % (object._meta.app_label,
                                     object._meta.object_name,)
 
+        model = get_vote_model(model_name)
+
+        total_votes = model.objects.filter(object_id=object_id).count()
+
+        up_votes = model.objects.filter(object_id=object_id,
+                                        value=1).count()
+        down_votes = model.objects.filter(object_id=object_id,
+                                        value= -1).count()
+
+        up_pct = float(up_votes) / float(total_votes)
+        down_pct = float(down_votes) / float(total_votes)
+
         dictionary = {'object': object,
-                      'model_name': model_name}
+                      'model_name': model_name,
+                      'up_pct': up_pct,
+                      'down_pct': down_pct}
 
         return render_to_string('django_votes/updownvote.html', dictionary, context_instance=context)
 
