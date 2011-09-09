2f0e7e1dc1a290b758ca5bf7dd03afbce7f8cf99
Maarten Timmerman <maarten.timmerman@city-live.be>
2011-09-09 10:42:13 +0200

    fixed a bug

--- src/django_votes/static/django_votes/js/base.js
+++ src/django_votes/static/django_votes/js/base.js
@@ -4,41 +4,45 @@ $(function(){
     $(document).bind('tabLoaded', load);
     $(document).bind('contentLoaded', load);
     
-    function load(){
-        $('.updownvotes').each(function(){
-            var id = $(this).attr('x:id');
-            var model_name = $(this).attr('x:model-name');
-            var result_url = $(this).attr('x:url');
-            var container = $(this);
+    $('.vote-up').live('click', function(){
+        var container = $(this).parents('.updownvotes:first');
+        var id = container.attr('x:id');
+        var model_name = container.attr('x:model-name');
+        var result_url = container.attr('x:url');
         
-            $(this).find('.vote-up').live('click', function(){
-                var url = $(this).attr('x:url');
+        var url = $(this).attr('x:url');
+
+        $.ajax({type:'POST',
+                url: url,
+                data: {'model': model_name, 'object_id': id},
+                success : function(data){
+                    container.replaceWith(data);
+                    container.find('.vote-results').slideDown();
+                }});
+
+        return false;
+    });
+    
+    $('.vote-down').live('click', function(){
+        var container = $(this).parents('.updownvotes:first');
+        var id = container.attr('x:id');
+        var model_name = container.attr('x:model-name');
+        var result_url = container.attr('x:url');
         
-                $.ajax({type:'POST',
-                        url: url,
-                        data: {'model': model_name, 'object_id': id},
-                        success : loadResults});
-        
-                return false;
-            });
-            $(this).find('.vote-down').live('click', function(){
-                var url = $(this).attr('x:url');
-        
-                $.ajax({type:'POST',
-                        url: url,
-                        data: {'model': model_name, 'object_id': id},
-                        success : loadResults});
-        
-                return false;
-            });
+        var url = $(this).attr('x:url');
+
+        $.ajax({type:'POST',
+                url: url,
+                data: {'model': model_name, 'object_id': id},
+                success : function(data){
+                    container.replaceWith(data);
+                    container.find('.vote-results').slideDown()
+                }});
+
+        return false;
+    });
             
-            function loadResults(data) {
-                container.replaceWith(data);
-                container.find('.vote-results').slideDown();
-                load();
-            }
-        });
-                
+    function load(){                        
         var rating_types = [gettext('Awful'),
                             gettext('Poor'),
                             gettext('Average'),
