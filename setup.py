f281e9760b457c06fdd0f9a0207916d6e3e9e745
Davide Setti <davide.setti@gmail.com>
2011-09-22 13:10:11 +0300

    errata

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="django-votes",
-    version=(0, 0, 1),
+    version="0.0.1",
     url='https://github.com/citylive/django-votes',
     license='BSD',
     description="Voting system for various things",
